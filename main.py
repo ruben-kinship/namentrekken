9c089fcbca2cbcccfa3928a1715d746de48ddaf8
Ruben <decosterruben@gmail.com>
2018-09-24 12:43:13 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+
+
+
+
+# verzamel namen van iedereen die meedoet (Ruben, Liselot, Ruth, ..)
+
+
+
+# Stel op welke paren er niet mogen bestaan (Ruben -> Liselot, Ruth -> Sebbe)
+
+
+
+# Vorm groepjes van 2, waarbij 1) niet naar zichzelf 2) niet met de paren van invalidpairs
+
+
+
+# print de bekomen lijst
+
+
+
